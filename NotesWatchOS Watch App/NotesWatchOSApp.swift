76674020fa1f90d758d0b5a5443a09dfb00282df
Lev Poznyakov <levfirstgamer@mail.ru>
2022-06-16 19:58:13 +0300

    Initial Commit

---
+++ NotesWatchOS Watch App/NotesWatchOSApp.swift
@@ -0,0 +1,14 @@
+// NotesWatchOSApp.swift
+
+import SwiftUI
+
+@main
+struct NotesWatchOS_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationStack {
+                ContentView()
+            }
+        }
+    }
+}
